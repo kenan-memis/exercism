b7e9cf27fad64980bac5909fbdff6ac493ee046a
Kenan Memis <kenanmemis@gmail.com>
2019-11-19 13:56:06 +0100

    rotational-cipher

---
+++ ruby/rotational-cipher/rotational_cipher.rb
@@ -0,0 +1,38 @@
+require 'pry'
+
+class RotationalCipher
+  ALPHABET = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
+
+  def self.rotate(str, rotation)    
+    str.chars.inject([]) do |arr, c|
+      arr << shift_string(c, rotation)
+      arr
+    end.join
+  end
+
+  private
+
+  def self.shift_string(char, rotate)
+    result = find_index_and_shifted_char(char, rotate)
+
+    return char unless result
+    
+    result[:shifted_char]
+  end
+
+  def self.find_index_and_shifted_char(char, rotate)
+    if char.match(/[a-z]/)
+      index = ALPHABET.index(char)
+      shift_size = (index + rotate) % 26
+
+      { index: index, shifted_char: ALPHABET[shift_size] }
+    elsif char.match(/[A-Z]/)
+      index = ALPHABET.map(&:upcase).index(char)
+      shift_size = (index + rotate) % 26
+
+      { index: index, shifted_char: ALPHABET.map(&:upcase)[shift_size] }
+    else
+      nil
+    end
+  end
+end
